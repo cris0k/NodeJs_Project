cf0847b522655b385d768f86ab813851aba57b37
Cristina Kukhar <criskukhar@hotmail.com>
2022-09-09 18:11:18 +0200

    Post product data done and working

--- routes/api/products.js
+++ routes/api/products.js
@@ -3,15 +3,38 @@ const router = express.Router();
 const {query, validationResult } = require('express-validator');
 const Product = require('/home/cris/Documents/Web13/Nodepop_Project/nodepop/models/Products.js');
 
+//GET /api/products
+// returns a list of products
+
 router.get('/', async (req, res, next) => {
     try {
       const products = await Product.find();
   
-      res.json({ results: products });
+      res.json({ adverts: products });
   
     } catch (err) {
       next(err);
     }
   });
+
+// POST /api/products (body)
+// creates an ad
+
+router.post('/', async (req, res, next) => {
+  try {
+    const advertData = req.body;
+
+    // instanciamos objeto en memoria
+    const product = new Product(advertData);
+
+    // lo guardamos en la base de datos
+    const productSaved = await product.save();
+
+    res.json({ result: productSaved });
+
+  } catch (error) {
+    next(error);
+  }
+});
 
   module.exports = router;
