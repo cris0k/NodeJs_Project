e957a0d09f07fae8a3c897b14d44614b9bbfa05c
Cristina Kukhar <criskukhar@hotmail.com>
2022-12-02 17:18:10 +0100

    change from ejs to html.header and index separated

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'html'); // usa un motor de vista custom, llamado 'html'
+app.engine('html', require('ejs').__express) // ese motor usa ejs
 
 require('./lib/connectMongoose.js');
 
